59c42ae5b7691c92197821ebaff922325dc98547
rabi-m <capital861@gmail.com>
2025-01-07 03:45:55 +0200

    add backend/frontend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('E-commerce Backend'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
